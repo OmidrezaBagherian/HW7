7d77b6ed64ac04efd948a932a059bb77016c0359
OmidrezaBagherian <62378531+OmidrezaBagherian@users.noreply.github.com>
2022-01-18 08:08:08 -0800

    Update LongestCommonPrefixString.kt

--- src/main/kotlin/soual1/LongestCommonPrefixString.kt
+++ src/main/kotlin/soual1/LongestCommonPrefixString.kt
@@ -1,18 +1,18 @@
 package soual1
 
-fun longestCommonPrefix(words: Array<String>): CharSequence {
+fun longestCommonPrefix(words: MutableList<String>): CharSequence? {
     val size = words.size
-    if (size == 0) return "There is no common prefix among the input strings"
+    if (size == 0) return "Null"
     if (size == 1) return words[0]
     words.sort()
     val end = words[0].length.coerceAtMost(words[size - 1].length)
     var i = 0
     while (i < end && words[0][i] == words[size - 1][i]) i++
-    return words[0].subSequence(0, i)
+    return if (words[0].subSequence(0, i)=="") null else words[0].subSequence(0, i)
 }
 
 fun main() {
-    val input = arrayOf("Omid", "Omidreza", "OmidX", "OmidV")
+    val input = mutableListOf<String>("a1pp","1apple","app1lication")
     println(longestCommonPrefix(input))
 }
 
